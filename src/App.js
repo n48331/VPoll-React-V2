b34dad674db7b4a7a19a3a0fac6ac498ae96544a
n48331 <unfolks@gmail.com>
2021-12-16 00:08:39 +0530

    15/12

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App()  {
   const [token, setToken] = useState();
   const [logged, setLogged] = useState();
 
+  const [polled, setPolled] = useState(0);
+  const [allCount, setAllCount] = useState(0);
+  const [list, setList] = useState();
+  const [notList, setnotList] = useState();
+
+
   useEffect(()=>{
       (
           
@@ -31,9 +37,31 @@ function App()  {
                   setLogged(false)
               }
 
+              let header = {
+                headers: {
+                  'Authorization': `token ${token}`
+                }
+              }
+              const interval = setInterval(() => {
+              axios.get('voters-list/',header).then(res=>{
+                const allData = res.data;
+                setList(allData)
+                  console.log(list);
+                const allPolled = allData.filter(item=>item.polled===true)
+                const allNotPolled = allData.filter(item=>item.polled===false)
+                setPolled(allPolled.length);
+                setAllCount(res.data.length);
+                setnotList(allNotPolled)
+    
+              }).catch(err=>{
+                  console.log('Error');
+              })
+            }, 3000);
+            return () => clearInterval(interval);
+
           }
       )()
-  })
+  },[])
     return ( 
       <div className = 'container-fluid'>
       <Router>
@@ -41,8 +69,11 @@ function App()  {
         <Routes>
         <Route/>
           <Route path = '/' element={<Login/>}/> 
-          <Route path = '/home' element={<Home logged={logged} token={token}/>}/> 
-          <Route path = '/notpolled' element={<Nopoll/>}/>
+          <Route path = '/home' 
+          element={<Home logged={logged} token={token}
+           list={list} polled={polled} allCount={allCount}
+            />}/> 
+          <Route path = '/notpolled' element={<Nopoll  notList={notList} />}/>
           <Route exact path = '/stats' element={<Stats/>}/>
           <Route exact path = '/login' element={<Login setLogged={setLogged}/>}/>
         </Routes>
--- src/Pages/Nopoll.js
+++ src/Pages/Nopoll.js
@@ -1,8 +1,97 @@
-import React from 'react';
-class Nopoll extends React.Component {
-    render() { 
-        return <div>Not Polled</div>;
+import React,{useEffect,useState} from "react";
+import axios from "axios";
+import { Accordion,Card,InputGroup,FormControl,ProgressBar } from 'react-bootstrap';
+import BackToTop from "react-back-to-top-button";
+import { ArrowUpCircleFill as Up } from 'react-bootstrap-icons';
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
+
+
+const Nopoll=(props)=>{
+    const [search, setSearch] = useState('');
+    const filterFunction = (item) => {
+      const searchAll=item.name+item.house+item.sl
+      return searchAll.toUpperCase().indexOf(search.toUpperCase()) > -1
     }
+
+    const pollFunction =(e)=>{
+      const vid = e.target.id
+      axios.patch(`voters-details/${vid}/`,{
+        "polled": e.target.checked
+      })
+
+    }
+
+    return(
+        <div>
+        <div className='row'>
+        <style>{'body { background-color: 	#000000; }'}</style>
+          <div className ='col-md-12'>
+      <InputGroup className="mb-3" material>
+
+    <FormControl
+    className='btn-dark p-2 mr2'
+      aria-label="Default"
+      aria-describedby="inputGroup-sizing-default"
+      placeholder="Search Something...."
+      value={search}
+      onChange={(e)=>{setSearch(e.target.value)}}
+    />
+
+  </InputGroup>
+
+          <Accordion >
+
+                {props.notList?
+                  props.notList.filter(filterFunction).map((itm,ky) => (
+                    <Card bg="primary" style={{ background: itm.polled ? '#3eaca8' : '#f67280', }}>
+    
+    <Accordion.Item  eventKey={itm.id} style={{ background: itm.polled ? '#3eaca8' : '#f67280' }}>
+    <Accordion.Header >
+    <Card.Body style={{ textDecorationLine: itm.polled ? 'line-through' : '',color:itm.polled ?'green':'red' }}>
+    <span className='btn-dark p-2 mr2'>{itm.sl}</span>
+     <b> <span className='text-uppercase'>
+      {itm.name}
+       </span> </b>
+       <span>
+        : {itm.house}
+       </span>
+       </Card.Body>
+       </Accordion.Header>
+       
+    <Accordion.Body>
+    Polled:
+      
+      <Toggle
+  id={ String(itm.id) }
+  checked={itm.polled}
+  onChange={pollFunction}    
+  /> 
+    </Accordion.Body>
+    
+  </Accordion.Item>
+  </Card>
+                )):"loading"
+                
+                }
+            </Accordion>
+
+
+          </div>
+          <BackToTop
+        showOnScrollUp
+        showAt={100}
+        speed={1500}
+        easing="easeInOutQuint"
+      >
+        <Up />
+      </BackToTop>       
+        </div>
+        </div>
+
+        
+    )
+
 }
- 
+
 export default Nopoll;
